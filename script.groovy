7bc01c37a02570b1db8e037114d37176048706c8
Aung Nyein Chan Oo <techwithnc.techwithnc@gmail.com>
2023-02-25 13:52:26 +0630

    add mvn build-helper command

--- script.groovy
+++ script.groovy
@@ -1,7 +1,10 @@
-def buildApp(){
-    sh 'ls -l'
-    sh 'docker build -t techwithnc/image:5.0 .'
-    sh 'docker image ls'
+def packApp(){
+    sh "mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit"
+    sh "mvn clean package"
+
+}
+def buildApp() {
+    sh 'docker build -t techwithnc/simple-maven-app:1.0 .'
 }
 def loginApp(){
     withCredentials([usernamePassword(credentialsId: 'dockerhub-token', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
