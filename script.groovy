d89f4836ea7e35088c31d55b45cac00b1d85e7fe
Aung Nyein Chan Oo <techwithnc.techwithnc@gmail.com>
2023-03-01 12:36:44 +0630

    change stages order

--- script.groovy
+++ script.groovy
@@ -19,7 +19,7 @@ def pushcode(){
                         sh 'git config user.name jenkins'
                         sh "git remote set-url origin https://${USERNAME}:${PASSWORD}@github.com/techwithnc/simple-java-maven-app.git"
                         sh 'git add .'
-                        sh 'git commit -m "update version to $NEW_APP_VERSION"'
+                        sh "git commit -m 'update version to $NEW_APP_VERSION'"
                         sh "echo $NEW_APP_VERSION"
                         sh 'git push origin HEAD:main'
     }
